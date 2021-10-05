a20e4f3b8b76d7c56c136f7a08b87c792f984798
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-05 17:25:23 +0200

    all countried display

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
 const colorMode = document.querySelector('.color-mode');
 const selectFilter = document.querySelector('.select-filter');
 const selectList = document.querySelector('.regions');
+const countriesMain = document.querySelector('.countries');
+
+const ALL_API = 'https://restcountries.com/v3.1/all';
+
+async function getCountries(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  displayCountries(data);
+}
+
+getCountries(ALL_API);
+
+function displayCountries(countries) {
+  countriesMain.innerHTML = '';
+  countries.forEach((country) => {
+    let { name, population, region, capital, flags } = country;
+    name = name.common;
+    capital = capital || 'unkown';
+    flags = flags.png;
+
+    let countryEl = document.createElement('div');
+    countryEl.classList.add('single-country');
+    countryEl.innerHTML = `
+    <img src="${flags}" alt="${name}" />
+          <div class="country-info">
+            <h1>${name}</h1>
+            <div class="country-details">
+              <p>Population: <span>${population}</span></p>
+              <p>Region: <span>${region}</span></p>
+              <p>Capital: <span>${capital}</span></p>
+            </div>
+          </div>
+    `;
+    countriesMain.appendChild(countryEl);
+  });
+}
 
 // Color toggle
 colorMode.addEventListener('click', (e) => {
