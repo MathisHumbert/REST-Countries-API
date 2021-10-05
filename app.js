6bb111be4e8b81f1d7246c1512da2b916dbca59a
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-05 16:52:30 +0200

    nav and filters done

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const colorMode = document.querySelector('.color-mode');
+const selectFilter = document.querySelector('.select-filter');
+const selectList = document.querySelector('.regions');
 
+// Color toggle
 colorMode.addEventListener('click', (e) => {
   document.body.classList.toggle('dark');
+});
+
+// Select event
+selectFilter.addEventListener('click', () => {
+  selectList.classList.toggle('show');
 });
