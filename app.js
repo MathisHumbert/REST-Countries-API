b3fb40985206c6f963d57d083f0291444104a0df
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-05 21:10:26 +0200

    single page created

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 const colorMode = document.querySelector('.color-mode');
-const selectFilter = document.querySelector('.select-filter');
+const selectFilter = document.querySelector('.filter-header');
 const selectList = document.querySelector('.regions');
 const countriesMain = document.querySelector('.countries');
+const form = document.getElementById('search-form');
+const textInput = document.getElementById('country-name');
 
 const ALL_API = 'https://restcountries.com/v3.1/all';
+const REGION_API = 'https://restcountries.com/v3.1/region/';
+const NAME_API = 'https://restcountries.com/v3.1/name/';
 
+// function accessing data
 async function getCountries(url) {
   const response = await fetch(url);
+  if (response.status === 404) {
+    return displayError();
+  }
   const data = await response.json();
 
+  // display the countries
   displayCountries(data);
 }
 
+// load all of the countries
 getCountries(ALL_API);
 
+// filter by region
+selectList.addEventListener('click', getTheRegion);
+
+function getTheRegion(e) {
+  if (e.target.classList.contains('show')) return;
+  getCountries(REGION_API + e.target.textContent);
+}
+
+// filter by name
+form.addEventListener('submit', getTheName);
+
+function getTheName(e) {
+  e.preventDefault();
+  getCountries(NAME_API + textInput.value);
+  this.reset();
+}
+
+// display the countries
 function displayCountries(countries) {
   countriesMain.innerHTML = '';
   countries.forEach((country) => {
@@ -22,12 +50,13 @@ function displayCountries(countries) {
     capital = capital || 'unkown';
     flags = flags.png;
 
-    let countryEl = document.createElement('div');
+    let countryEl = document.createElement('a');
     countryEl.classList.add('single-country');
+    countryEl.setAttribute('href', 'country.html');
     countryEl.innerHTML = `
     <img src="${flags}" alt="${name}" />
           <div class="country-info">
-            <h1>${name}</h1>
+            <h1 class="country-name">${name}</h1>
             <div class="country-details">
               <p>Population: <span>${population}</span></p>
               <p>Region: <span>${region}</span></p>
@@ -48,3 +77,8 @@ colorMode.addEventListener('click', (e) => {
 selectFilter.addEventListener('click', () => {
   selectList.classList.toggle('show');
 });
+
+// Display Error
+function displayError() {
+  alert('no such country');
+}
