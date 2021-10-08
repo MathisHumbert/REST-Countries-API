ff26686522478275352e7f5836dfed2dc398a59c
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-08 15:01:28 +0200

    project is done

--- utils/colorLoad.js
+++ utils/colorLoad.js
@@ -1,13 +1,12 @@
 function colorLoad() {
   let bgColor = '';
+  let html = document.querySelector('html');
 
   if (!localStorage.getItem('color')) bgColor = 'white';
   else bgColor = localStorage.getItem('color');
 
-  console.log(bgColor);
-
-  if (bgColor === 'white') document.body.classList.remove('dark');
-  else document.body.classList.add('dark');
+  if (bgColor === 'white') html.classList.remove('dark');
+  else html.classList.add('dark');
 }
 
 export default colorLoad;
--- utils/colorToggle.js
+++ utils/colorToggle.js
@@ -1,12 +1,13 @@
 // Color toggle function
 const colorMode = document.querySelector('.color-mode');
+let html = document.querySelector('html');
 
 let color = colorMode.addEventListener('click', (e) => {
-  if (document.body.classList.contains('dark')) {
-    document.body.classList.remove('dark');
+  if (html.classList.contains('dark')) {
+    html.classList.remove('dark');
     localStorage.setItem('color', 'white');
   } else {
-    document.body.classList.add('dark');
+    html.classList.add('dark');
     localStorage.setItem('color', 'dark');
   }
 });
